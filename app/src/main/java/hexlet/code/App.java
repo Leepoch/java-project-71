5820b9eba4b93e071cb2fa00ce93ecd85c9e8280
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-04-02 17:31:23 +0300

    add picocli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,19 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
+        description = "Print the difference between two files")
+
+public class App implements Callable<Integer> {
+    public Integer call() {
+        return 0;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
