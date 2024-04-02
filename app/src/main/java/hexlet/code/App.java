c83051dc9a08aa8d3d805784de1dc18933005138
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-04-02 19:26:37 +0300

    add param

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,15 +1,26 @@
 package hexlet.code;
 
 import picocli.CommandLine;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "app", mixinStandardHelpOptions = true, version = "app 1.0",
-        description = "Print the difference between two files")
+        description = "Compares two configuration files and shows a difference.")
 
 public class App implements Callable<Integer> {
-    public Integer call() {
+    @Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    @Override
+    public Integer call() throws Exception {
         return 0;
     }
     public static void main(String[] args) {
