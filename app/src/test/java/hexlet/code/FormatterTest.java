21809579ee9f24db271c5a5b433da1e41d17aa0a
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-04-10 16:14:16 +0300

    fix expected value

--- app/src/test/java/hexlet/code/FormatterTest.java
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -8,44 +8,46 @@ import static hexlet.code.Formatter.generate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormatterTest {
-    private final String EXPECTED_VALUE_STYLISH = "{\n" +
-            "    chars1: [a, b, c]\n" +
-            "  - chars2: [d, e, f]\n" +
-            "  + chars2: false\n" +
-            "  - checked: false\n" +
-            "  + checked: true\n" +
-            "  - default: null\n" +
-            "  + default: [value1, value2]\n" +
-            "  - id: 45\n" +
-            "  + id: null\n" +
-            "  - key1: value1\n" +
-            "  + key2: value2\n" +
-            "    numbers1: [1, 2, 3, 4]\n" +
-            "  - numbers2: [2, 3, 4, 5]\n" +
-            "  + numbers2: [22, 33, 44, 55]\n" +
-            "  - numbers3: [3, 4, 5]\n" +
-            "  + numbers4: [4, 5, 6]\n" +
-            "  + obj1: {nestedKey=value, isNested=true}\n" +
-            "  - setting1: Some value\n" +
-            "  + setting1: Another value\n" +
-            "  - setting2: 200\n" +
-            "  + setting2: 300\n" +
-            "  - setting3: true\n" +
-            "  + setting3: none\n" +
-            "}";
-    private final String EXPECTED_VALUE_PLAIN = "Property 'chars2' was updated. From [complex value] to false\n" +
-            "Property 'checked' was updated. From false to true\n" +
-            "Property 'default' was updated. From null to [complex value]\n" +
-            "Property 'id' was updated. From 45 to null\n" +
-            "Property 'key1' was removed\n" +
-            "Property 'key2' was added with value: 'value2'\n" +
-            "Property 'numbers2' was updated. From [complex value] to [complex value]\n" +
-            "Property 'numbers3' was removed\n" +
-            "Property 'numbers4' was added with value: [complex value]\n" +
-            "Property 'obj1' was added with value: [complex value]\n" +
-            "Property 'setting1' was updated. From 'Some value' to 'Another value'\n" +
-            "Property 'setting2' was updated. From 200 to 300\n" +
-            "Property 'setting3' was updated. From true to 'none'";
+    private final String EXPECTED_VALUE_STYLISH = """
+            {
+                chars1: [a, b, c]
+              - chars2: [d, e, f]
+              + chars2: false
+              - checked: false
+              + checked: true
+              - default: null
+              + default: [value1, value2]
+              - id: 45
+              + id: null
+              - key1: value1
+              + key2: value2
+                numbers1: [1, 2, 3, 4]
+              - numbers2: [2, 3, 4, 5]
+              + numbers2: [22, 33, 44, 55]
+              - numbers3: [3, 4, 5]
+              + numbers4: [4, 5, 6]
+              + obj1: {nestedKey=value, isNested=true}
+              - setting1: Some value
+              + setting1: Another value
+              - setting2: 200
+              + setting2: 300
+              - setting3: true
+              + setting3: none
+            }""";
+    private final String EXPECTED_VALUE_PLAIN = """
+            Property 'chars2' was updated. From [complex value] to false
+            Property 'checked' was updated. From false to true
+            Property 'default' was updated. From null to [complex value]
+            Property 'id' was updated. From 45 to null
+            Property 'key1' was removed
+            Property 'key2' was added with value: 'value2'
+            Property 'numbers2' was updated. From [complex value] to [complex value]
+            Property 'numbers3' was removed
+            Property 'numbers4' was added with value: [complex value]
+            Property 'obj1' was added with value: [complex value]
+            Property 'setting1' was updated. From 'Some value' to 'Another value'
+            Property 'setting2' was updated. From 200 to 300
+            Property 'setting3' was updated. From true to 'none'""";
     @Test
     public void testDiffJsonStylish() throws Exception {
         String resourceName1 = "file1.json";
