976d9fd8de188581bfb75f83f6b06cc5b8024363
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-05-10 13:54:57 +0300

    update FormatterTest

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -1,5 +1,6 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
@@ -9,15 +10,22 @@ import java.util.Map;
 
 public class Parser {
     public static Map<String, Object> parse(String data, String type) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        YAMLMapper yamlMapper = new YAMLMapper();
         var parsedData = new HashMap<String, Object>();
         if (type.equals("json")) {
-            parsedData = objectMapper.readValue(data, new TypeReference<>() { });
+            parsedData = jsonReader(data);
         } else if (type.equals("yml") || type.equals("yaml")) {
-            parsedData = yamlMapper.readValue(data, new TypeReference<>() { });
+            parsedData = yamlReader(data);
         }
         return parsedData;
     }
 
+    public static HashMap<String, Object> jsonReader(String data) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(data, new TypeReference<>() { });
+    }
+
+    public static HashMap<String, Object> yamlReader(String data) throws JsonProcessingException {
+        YAMLMapper yamlMapper = new YAMLMapper();
+        return yamlMapper.readValue(data, new TypeReference<>() { });
+    }
 }
--- app/src/test/java/hexlet/code/FormatterTest.java
+++ app/src/test/java/hexlet/code/FormatterTest.java
@@ -2,124 +2,121 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FormatterTest {
-    private final String expectedValueStylish = """
-            {
-                chars1: [a, b, c]
-              - chars2: [d, e, f]
-              + chars2: false
-              - checked: false
-              + checked: true
-              - default: null
-              + default: [value1, value2]
-              - id: 45
-              + id: null
-              - key1: value1
-              + key2: value2
-                numbers1: [1, 2, 3, 4]
-              - numbers2: [2, 3, 4, 5]
-              + numbers2: [22, 33, 44, 55]
-              - numbers3: [3, 4, 5]
-              + numbers4: [4, 5, 6]
-              + obj1: {nestedKey=value, isNested=true}
-              - setting1: Some value
-              + setting1: Another value
-              - setting2: 200
-              + setting2: 300
-              - setting3: true
-              + setting3: none
-            }""";
-    private final String expectedValuePlain = """
-            Property 'chars2' was updated. From [complex value] to false
-            Property 'checked' was updated. From false to true
-            Property 'default' was updated. From null to [complex value]
-            Property 'id' was updated. From 45 to null
-            Property 'key1' was removed
-            Property 'key2' was added with value: 'value2'
-            Property 'numbers2' was updated. From [complex value] to [complex value]
-            Property 'numbers3' was removed
-            Property 'numbers4' was added with value: [complex value]
-            Property 'obj1' was added with value: [complex value]
-            Property 'setting1' was updated. From 'Some value' to 'Another value'
-            Property 'setting2' was updated. From 200 to 300
-            Property 'setting3' was updated. From true to 'none'""";
-    private final String expectedValueJson = """
-            {"type":"notChanged","value":["a","b","c"],"key":"chars1"}
-            {"value2":false,"value1":["d","e","f"],"type":"changed","key":"chars2"}
-            {"value2":true,"value1":false,"type":"changed","key":"checked"}
-            {"value2":["value1","value2"],"value1":null,"type":"changed","key":"default"}
-            {"value2":null,"value1":45,"type":"changed","key":"id"}
-            {"type":"deleted","value":"value1","key":"key1"}
-            {"type":"added","value":"value2","key":"key2"}
-            {"type":"notChanged","value":[1,2,3,4],"key":"numbers1"}
-            {"value2":[22,33,44,55],"value1":[2,3,4,5],"type":"changed","key":"numbers2"}
-            {"type":"deleted","value":[3,4,5],"key":"numbers3"}
-            {"type":"added","value":[4,5,6],"key":"numbers4"}
-            {"type":"added","value":{"nestedKey":"value","isNested":true},"key":"obj1"}
-            {"value2":"Another value","value1":"Some value","type":"changed","key":"setting1"}
-            {"value2":300,"value1":200,"type":"changed","key":"setting2"}
-            {"value2":"none","value1":true,"type":"changed","key":"setting3"}
-            """;
+    @Test
+    public void testDiffJsonWithoutFormat() throws Exception {
+        String resourceName1 = "file1.json";
+        String resourceName2 = "file2.json";
+        String expectedValueFileName = "testExpectedStylish";
+        String absolutePath1 = createAbsolutePath(resourceName1);
+        String absolutePath2 = createAbsolutePath(resourceName2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
+        var actual = Differ.generate(absolutePath1, absolutePath2);
+        assertEquals(expectedValue, actual);
+    }
+
+    @Test
+    public void testDiffYmlWithoutFormat() throws Exception {
+        String resourceName1 = "file1.yml";
+        String resourceName2 = "file2.yml";
+        String expectedValueFileName = "testExpectedStylish";
+        String absolutePath1 = createAbsolutePath(resourceName1);
+        String absolutePath2 = createAbsolutePath(resourceName2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
+        var actual = Differ.generate(absolutePath1, absolutePath2);
+        assertEquals(expectedValue, actual);
+    }
+
     @Test
     public void testDiffJsonStylish() throws Exception {
         String resourceName1 = "file1.json";
         String resourceName2 = "file2.json";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file1 = new File(classLoader.getResource(resourceName1).getFile());
-        File file2 = new File(classLoader.getResource(resourceName2).getFile());
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        String expectedValueFileName = "testExpectedStylish";
+        String absolutePath1 = createAbsolutePath(resourceName1);
+        String absolutePath2 = createAbsolutePath(resourceName2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
         var actual = Differ.generate(absolutePath1, absolutePath2, "stylish");
-        assertEquals(expectedValueStylish, actual);
+        assertEquals(expectedValue, actual);
     }
     @Test
     public void testDiffYmlStylish() throws Exception {
         String resourceNameYml1 = "file1.yml";
         String resourceNameYml2 = "file2.yml";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file1 = new File(classLoader.getResource(resourceNameYml1).getFile());
-        File file2 = new File(classLoader.getResource(resourceNameYml2).getFile());
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        String expectedValueFileName = "testExpectedStylish";
+        String absolutePath1 = createAbsolutePath(resourceNameYml1);
+        String absolutePath2 = createAbsolutePath(resourceNameYml2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
         var actual = Differ.generate(absolutePath1, absolutePath2, "stylish");
-        assertEquals(expectedValueStylish, actual);
+        assertEquals(expectedValue, actual);
     }
     @Test
     public void testDiffJsonPlain() throws Exception {
-        String resourceNameYml1 = "file1.yml";
-        String resourceNameYml2 = "file2.yml";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file1 = new File(classLoader.getResource(resourceNameYml1).getFile());
-        File file2 = new File(classLoader.getResource(resourceNameYml2).getFile());
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        String resourceNameYml1 = "file1.json";
+        String resourceNameYml2 = "file2.json";
+        String expectedValueFileName = "testExpectedPlain";
+        String absolutePath1 = createAbsolutePath(resourceNameYml1);
+        String absolutePath2 = createAbsolutePath(resourceNameYml2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
         var actual = Differ.generate(absolutePath1, absolutePath2, "plain");
-        assertEquals(expectedValuePlain, actual);
+        assertEquals(expectedValue, actual);
     }
     @Test
     public void testDiffYmlPlain() throws Exception {
         String resourceNameYml1 = "file1.yml";
         String resourceNameYml2 = "file2.yml";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file1 = new File(classLoader.getResource(resourceNameYml1).getFile());
-        File file2 = new File(classLoader.getResource(resourceNameYml2).getFile());
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        String expectedValueFileName = "testExpectedPlain";
+        String absolutePath1 = createAbsolutePath(resourceNameYml1);
+        String absolutePath2 = createAbsolutePath(resourceNameYml2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
         var actual = Differ.generate(absolutePath1, absolutePath2, "plain");
-        assertEquals(expectedValuePlain, actual);
+        assertEquals(expectedValue, actual);
     }
     @Test
     public void testDiffJsonInJson() throws Exception {
+        String resourceNameYml1 = "file1.json";
+        String resourceNameYml2 = "file2.json";
+        String expectedValueFileName = "testExpectedJson";
+        String absolutePath1 = createAbsolutePath(resourceNameYml1);
+        String absolutePath2 = createAbsolutePath(resourceNameYml2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
+        var actual = Differ.generate(absolutePath1, absolutePath2, "json");
+        assertEquals(expectedValue, actual);
+    }
+
+    @Test
+    public void testDiffYmlInJson() throws Exception {
         String resourceNameYml1 = "file1.yml";
         String resourceNameYml2 = "file2.yml";
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file1 = new File(classLoader.getResource(resourceNameYml1).getFile());
-        File file2 = new File(classLoader.getResource(resourceNameYml2).getFile());
-        String absolutePath1 = file1.getAbsolutePath();
-        String absolutePath2 = file2.getAbsolutePath();
+        String expectedValueFileName = "testExpectedJson";
+        String absolutePath1 = createAbsolutePath(resourceNameYml1);
+        String absolutePath2 = createAbsolutePath(resourceNameYml2);
+        String absolutePathExpectedValue = createAbsolutePath(expectedValueFileName);
+        var expectedValue = fileReader(absolutePathExpectedValue);
         var actual = Differ.generate(absolutePath1, absolutePath2, "json");
-        assertEquals(expectedValueJson, actual);
+        assertEquals(expectedValue, actual);
+    }
+
+    public static String fileReader(String filepath) throws Exception {
+        Path absolutePath1 = Paths.get(filepath).toAbsolutePath().normalize();
+        return Files.readString(absolutePath1);
+    }
+
+    public static String createAbsolutePath(String resourceName) {
+        ClassLoader classLoader = FormatterTest.class.getClassLoader();
+        File file = new File(classLoader.getResource(resourceName).getFile());
+        return file.getAbsolutePath();
     }
 }
