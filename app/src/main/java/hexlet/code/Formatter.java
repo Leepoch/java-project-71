97db0820e35b0ab6e8ea8bc6592ff5adb7a58290
Daniil Tinkov <daniiltinkovv@gmail.com>
2024-05-14 13:14:05 +0300

    update tree

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import hexlet.code.formatters.FormatterJson;
 import hexlet.code.formatters.FormatterPlain;
 import hexlet.code.formatters.FormatterStylish;
+
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -11,12 +13,12 @@ public class Formatter {
     public static String chooseFormatter(
             ArrayList<HashMap<String, Object>> diff,
             String format
-    ) throws JsonProcessingException {
+    ) throws IOException {
         return switch (format) {
             case "stylish" -> FormatterStylish.formatter(diff);
             case "plain" -> FormatterPlain.formatter(diff);
             case "json" -> FormatterJson.formatter(diff);
-            default -> "";
+            default -> throw new IOException();
         };
     }
 }
--- app/src/main/java/hexlet/code/Tree.java
+++ app/src/main/java/hexlet/code/Tree.java
@@ -1,9 +1,6 @@
 package hexlet.code;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Tree {
     public static ArrayList<HashMap<String, Object>> generateTree(
@@ -18,7 +15,20 @@ public class Tree {
         var sortedMergedData = new TreeMap<>(mergedData);
         sortedMergedData.forEach((key, value) -> {
             if (parsedData1.containsKey(key) && parsedData2.containsKey(key)) {
-                comparingObjects(parsedData1, parsedData2, diff, key, value);
+                var valueFieldFile1 = String.valueOf(parsedData1.get(key));
+                var valueFieldFile2 = String.valueOf(parsedData2.get(key));
+                var typeWithValue = new HashMap<String, Object>();
+                if (isEqual(valueFieldFile1, valueFieldFile2)) {
+                    typeWithValue.put("type", "notChanged");
+                    typeWithValue.put("value", value);
+                    typeWithValue.put("key", key);
+                } else {
+                    typeWithValue.put("type", "changed");
+                    typeWithValue.put("key", key);
+                    typeWithValue.put("value1", parsedData1.get(key));
+                    typeWithValue.put("value2", value);
+                }
+                diff.add(typeWithValue);
             } else if (parsedData1.containsKey(key) && !parsedData2.containsKey(key)) {
                 var typeWithValue = new HashMap<String, Object>();
                 typeWithValue.put("type", "deleted");
@@ -36,28 +46,10 @@ public class Tree {
         return diff;
     }
 
-    public static void comparingObjects(
-            Map<String, Object> parsedData1,
-            Map<String, Object> parsedData2,
-            ArrayList<HashMap<String, Object>> diff,
-            String key,
-            Object value
-    ) {
-        var valueFieldFile1 = String.valueOf(parsedData1.get(key));
-        var valueFieldFile2 = String.valueOf(parsedData2.get(key));
-        if (valueFieldFile1.equals(valueFieldFile2)) {
-            var typeWithValue = new HashMap<String, Object>();
-            typeWithValue.put("type", "notChanged");
-            typeWithValue.put("value", value);
-            typeWithValue.put("key", key);
-            diff.add(typeWithValue);
-        } else {
-            var typeWithValue = new HashMap<String, Object>();
-            typeWithValue.put("type", "changed");
-            typeWithValue.put("key", key);
-            typeWithValue.put("value1", parsedData1.get(key));
-            typeWithValue.put("value2", value);
-            diff.add(typeWithValue);
+    private static Boolean isEqual(Object value1, Object value2) {
+        if (value1 == null || value2 == null) {
+            return value1 == value2;
         }
+        return value1.equals(value2);
     }
 }
